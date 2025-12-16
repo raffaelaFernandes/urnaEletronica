0c97b9b57de3b11e11a8f8f02b84ab6e73f4c123
Raffaela Fernandes <161386357+raffaelaFernandes@users.noreply.github.com>
2025-12-16 15:08:38 -0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -85,7 +85,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     if (inicio) {
         inicio.addEventListener("click", () => {
-            window.location.href = "filho.html";
+            window.location.href = "index.html";
         });
     }
 
@@ -112,7 +112,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 localStorage.removeItem("ultimaCategoria");
                 sessionStorage.removeItem("acessoResultados");
 
-                window.location.href = "filho.html";
+                window.location.href = "index.html";
             }
         });
     }
